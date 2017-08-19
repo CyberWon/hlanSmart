efad45bc7e53e0d5b645e053a992c698fb3cc24d
iyuesh <iyuesh@163.com>
2017-08-19 15:31:53 +0800

    --add.js跟接口交互的文件

--- static/hlan.js
+++ static/hlan.js
@@ -1,39 +1,19 @@
 hlan = {};
+// debug 开启时在控制台打印日志
 hlan.debug=true
 hlan.res = {}
 hlan.menu = {}
+// 自己构建日志打印，方便集中控制
 hlan.log=function(s){
 	hlan.debug && console.log(s)
 }
 
-hlan.send = function(retfun, method, params) {
-	p = {};
-	if(typeof(method) != "undefined" && method != '') {
-		p.method = method;
-	} else {
-		hlan.log('method error');
-		return;
-	}
-	if(typeof(params) == 'undefined') {
-		params = {}
-	}
-	p.retfun = retfun;
-	p.params = JSON.stringify(params);
-	p.id = new Date().getTime();
-	try {
-		$.post(hlan.api_url, p, function(data, status) {
-			msg = JSON.parse(data)
-			hlan.res[msg.id] = msg.retdata
-			eval(msg.retfun + '("' + msg.id + '")')
-		});
-	} catch(err) {
-		hlan.log('-----ws_send websokect发送报错');
-		hlan.log(err);
-	}
-	return ''
 
-}
+
 hlan.app_send = function(retfun,app, method, params) {
+	/*
+	 
+	 */
 	p = {};
 	if(typeof(method) != "undefined" && method != '') {
 		p.method = method;
@@ -47,11 +27,19 @@ hlan.app_send = function(retfun,app, method, params) {
 	p.retfun = retfun;
 	p.params = JSON.stringify(params);
 	p.id = new Date().getTime();
-
 	try {
 		$.post('/app/'+app, p, function(data, status) {
 			msg = JSON.parse(data)
 			hlan.res[msg.id] = msg.retdata
+			/*
+			 回调函数，所有的和接口交互都可以使用这个。
+			 onclick="hlan.app_send('函数名','app名','app模块名','app模块需要的参数')"
+			 接受函数需要从浏览器存储取出来数据
+			 exp:
+			 funcation test(id){
+			 	hlan.log(hlan.res[id])
+			 }
+			 */
 			eval(msg.retfun + '("' + msg.id + '")')
 		});
 	} catch(err) {
@@ -59,40 +47,7 @@ hlan.app_send = function(retfun,app, method, params) {
 		hlan.log(err);
 	}
 	return ''
-}
 
-function getmenu() {
-	$.get('/api/menu/menu1', function(res) {
-		hlan.menu = JSON.parse(res);
-		writemenu()
-	})
-}
-
-function navmenuClick(o) {
-	hlan.send('writemenu', 'menu.getmenu', {
-		'navid': o
-	})
-}
-
-function getnav(d) {
-	var s = '';
-	first = 0;
-	for(i in hlan.res[d]) {
-		if(first == 0) {
-			first = 1
-			hlan.send('writemenu', 'menu.getmenu', {
-				'navid': hlan.res[d][i][0]
-			})
-		}
-		if (hlan.res[d][i][3]!="#"){
-		s = s + "<li><a class=\"J_menuItem\" href=\"" + hlan.res[d][i][3] + "\"><i class=\"fa " + hlan.res[d][i][2] + "\"></i>" + hlan.res[d][i][1] + " </a></li>";
-		}else{
-			s = s + "<li><a  href=\"javascript:navmenuClick('" + hlan.res[d][i][0] + "')\"><i class=\"fa " + hlan.res[d][i][2] + "\"></i>" + hlan.res[d][i][1] + " </a></li>";
-		}
-	}
-	$('#nav_bar').html(s)
-	hlan.initmenu()
-	delete hlan.res[d]
 }
 var ws;
 hlan.websocket = function() {
@@ -111,17 +66,28 @@ hlan.websocket = function() {
 			console.log('接受的数据格式不正确')
 		}
 		try {
-			eval(msg.retfun + '("' + msg.retdata + '")')
+			id=new Date().getTime();
+			hlan.res[id]=msg.retdata
+			/*
+			 监听函数，用来接受服务端返回的数据
+			 服务端利用websocket.send(data)发送
+			 接受函数需要从浏览器存储取出来数据
+			 exp:
+			 funcation test(id){
+			 	hlan.log(hlan.res[id])
+			 }
+			 */
+			eval(msg.retfun + '("' + id + '")')
 		} catch(err) {
-			console.log(err)
-			console.log('执行' + msg.retfun + '失败')
+			hlan.log(err)
+			hlan.log('执行' + msg.retfun + '失败')
 		}
 
 	};
 
 	ws.onclose = function() {
 		// 关闭 websocket
-        hlan.debug && console.log('websocket断开链接了')
+        hlan.log('websocket断开链接了')
 
 	};
 }
@@ -150,42 +116,4 @@ hlan.ws_send = function(retfun, method, params) {
 		console.log(err);
 	}
 
-}
-//IP转成整型
-function _ip2int(ip) {
-	var num = 0;
-	ip = ip.split(".");
-	num = Number(ip[0]) * 256 * 256 * 256 + Number(ip[1]) * 256 * 256 + Number(ip[2]) * 256 + Number(ip[3]);
-	num = num >>> 0;
-	return num;
-};
-//整型解析为IP地址
-function _int2ip(num) {
-	var str;
-	var tt = new Array();
-	tt[0] = (num >>> 24) >>> 0;
-	tt[1] = ((num << 8) >>> 24) >>> 0;
-	tt[2] = (num << 16) >>> 24;
-	tt[3] = (num << 24) >>> 24;
-	str = String(tt[0]) + "." + String(tt[1]) + "." + String(tt[2]) + "." + String(tt[3]);
-	return str;
-};
-function bytesToSize(bytes) {
-	if(bytes === 0) return '0 B';
-	var k = 1024, // or 1024
-		sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-		i = Math.floor(Math.log(bytes) / Math.log(k));
-
-	return(bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
-}
-
-function isEmptyObject(obj) {
-	for(var key in obj) {
-		//	console.log(key);
-		return false;
-	}
-	return true;
-}
-hlan.login=function () {
-    window.location.href=hlan.login_url+'?url='+window.location.pathname;
 }
